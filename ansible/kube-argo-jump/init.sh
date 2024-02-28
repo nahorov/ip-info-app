27706ce8ce14682fd8415aea209a42825e12b94e
nahorov <rohanpuranik1996@gmail.com>
2024-02-28 11:03:21 +0530

    new key

--- ansible/kube-argo-jump/init.sh
+++ ansible/kube-argo-jump/init.sh
@@ -4,8 +4,8 @@
 sudo yum update -y
 
 # Install necessary packages
-sudo yum install python3 python3-pip -y
-sudo pip3 install paramiko ansible
+sudo yum install python3 python3-pip git -y
+sudo pip3 install ansible
 
 # Create a new user "ancon" with password "ancon"
 sudo useradd -m -s /bin/bash ancon
@@ -17,25 +17,29 @@ echo "ancon ALL=(ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoers > /dev/null
 # Switch to "ancon" user
 sudo su - ancon << EOF
 
-# Generate SSH key pair
-ssh-keygen -t rsa -b 2048 -N "" -f ~/.ssh/id_rsa-1
-
-# Define IP addresses
-java_jenkins_maven_ip="10.0.1.6"
-nexus_ip="10.0.2.5"
-
-# Copy SSH public key to java_jenkins_maven_ip and nexus_ip
-ssh-copy-id -i ~/.ssh/id_rsa-1.pub ancon@${java_jenkins_maven_ip}
-ssh-copy-id -i ~/.ssh/id_rsa-1.pub ancon@${nexus_ip}
-
 # Clone the repository
 git clone https://github.com/nahorov/ip-info-app.git /tmp/ip-info-app
+
+# Pull the key-pair file
+cp /tmp/ip-info-app/terraform/20240228.pem ~/.ssh/20240228.pem
+chmod 400 ~/.ssh/20240228.pem
+
+# Define inventory file
+cat <<EOF > inventory.ini
+[all]
+java_jenkins_maven ansible_host=10.0.1.6
+nexus ansible_host=10.0.2.5
+
+[all:vars]
+ansible_user=ec2-user
+ansible_ssh_private_key_file=~/.ssh/20240228.pem
+EOF
 
 # Remove the terraform and ip-info-app folders
 rm -rf /tmp/ip-info-app/terraform /tmp/ip-info-app/ip-info-app
 
 # Run the playbook
-ansible-playbook /tmp/ip-info-app/ansible/playbook.yml
+ansible-playbook -i inventory.ini /tmp/ip-info-app/ansible/playbook.yml
 
 EOF
 
