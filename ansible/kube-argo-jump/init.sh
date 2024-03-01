e23ee1eca160a8f80ce6bbf48495d8ae88fc948a
nahorov <rohanpuranik1996@gmail.com>
2024-03-01 14:37:56 +0530

    31

--- ansible/kube-argo-jump/init.sh
+++ ansible/kube-argo-jump/init.sh
@@ -7,15 +7,15 @@ sudo yum update -y
 sudo yum install git python3 python3-pip -y
 sudo pip3 install paramiko ansible
 
-# Pull the repository
-git clone -b master https://github.com/nahorov/ip-info-app.git ~/ip-info-app
+# Pull the repository to /tmp
+git clone -b master https://github.com/nahorov/ip-info-app.git /tmp/ip-info-app
 
 # Pull the key-pair file
-cp ~/ip-info-app/terraform/20240228.pem ~/.ssh/20240228.pem || { echo "Error: Copying key-pair file failed"; exit 1; }
+cp /tmp/ip-info-app/terraform/20240228.pem ~/.ssh/20240228.pem || { echo "Error: Copying key-pair file failed"; exit 1; }
 chmod 400 ~/.ssh/20240228.pem || { echo "Error: Setting permissions on key-pair file failed"; exit 1; }
 
 # Define inventory file
-cat <<EOF > ~/inventory.ini
+cat <<EOF > ~/.ssh/inventory.ini
 [all]
 java_jenkins_maven ansible_host=10.0.1.6
 nexus ansible_host=10.0.2.5
@@ -27,7 +27,7 @@ ansible_ssh_common_args="-o StrictHostKeyChecking=no"
 EOF
 
 # Remove the terraform and ip-info-app folders
-rm -rf ~/ip-info-app/terraform ~/ip-info-app/ip-info-app || { echo "Error: Removing folders failed"; exit 1; }
+rm -rf /tmp/ip-info-app/terraform /tmp/ip-info-app/ip-info-app || { echo "Error: Removing folders failed"; exit 1; }
 
 # Download Java JDK Corretto 17
 wget -O /tmp/java.tar.gz https://corretto.aws/downloads/latest/amazon-corretto-17-x64-linux-jdk.tar.gz
@@ -36,9 +36,9 @@ wget -O /tmp/java.tar.gz https://corretto.aws/downloads/latest/amazon-corretto-1
 wget -O /tmp/nexus.tar.gz https://download.sonatype.com/nexus/3/latest-unix.tar.gz
 
 # Run the playbooks
-ansible-playbook -i ~/inventory.ini ~/ip-info-app/ansible/java-jenkins-maven/java-jenkins-maven.yml -vvv
-ansible-playbook -i ~/inventory.ini ~/ip-info-app/ansible/nexus/nexus.yml -vvv
-ansible-playbook -i ~/inventory.ini ~/ip-info-app/ansible/kube-argo-jump/kube-argo-jump.yml -vvv
+ansible-playbook -i ~/.ssh/inventory.ini /tmp/ip-info-app/ansible/java-jenkins-maven/java-jenkins-maven.yml -vvv
+ansible-playbook -i ~/.ssh/inventory.ini /tmp/ip-info-app/ansible/nexus/nexus.yml -vvv
+ansible-playbook -i ~/.ssh/inventory.ini /tmp/ip-info-app/ansible/kube-argo-jump/kube-argo-jump.yml -vvv
 
 echo "Setup completed successfully."
 
