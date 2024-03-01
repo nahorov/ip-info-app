326e69d1cdf2811a5d05acc5ac2f967a7948c6de
nahorov <rohanpuranik1996@gmail.com>
2024-03-01 13:06:41 +0530

    26

--- ansible/kube-argo-jump/init.sh
+++ ansible/kube-argo-jump/init.sh
@@ -7,6 +7,9 @@ sudo yum update -y
 sudo yum install git python3 python3-pip -y
 sudo pip3 install paramiko ansible
 
+# Add Ansible to PATH for the ancon user
+sudo -u ancon bash -c 'echo "export PATH=$PATH:/usr/local/bin" >> ~/.bashrc'
+
 # Pull the repository
 git clone -b master https://github.com/nahorov/ip-info-app.git /tmp/ip-info-app
 
@@ -16,5 +19,4 @@ echo "ancon:ancon" | sudo chpasswd
 
 # Grant sudoer access to "ancon"
 echo "ancon ALL=(ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoers > /dev/null
-
 
