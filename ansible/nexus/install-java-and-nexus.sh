b1370be8c56e2dd21b15acf8843d69e287b71267
nahorov <rohanpuranik1996@gmail.com>
2024-03-01 16:43:45 +0530

    34

--- ansible/nexus/install-java-and-nexus.sh
+++ ansible/nexus/install-java-and-nexus.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
+# Function to install Java
+install_java() {
+  echo "Installing Java..."
+  tar -xf /tmp/java.tar.gz -C /opt/
+  echo 'export JAVA_HOME=/opt/java' >> /etc/profile
+  source /etc/profile
+}
+
+# Function to install Nexus
+install_nexus() {
+  echo "Installing Nexus..."
+  tar -xf /tmp/nexus.tar.gz -C /opt/
+}
+
 # Function to configure Nexus with Jenkins
 configure_nexus_with_jenkins() {
+  echo "Configuring Nexus with Jenkins..."
   jenkins_url="http://10.0.1.6:8080"
   nexus_url="http://10.0.2.5:8082"
   jenkins_username="admin"
@@ -25,35 +40,17 @@ configure_nexus_with_jenkins() {
   " "$jenkins_url/scriptText"
 }
 
-# Function to install Java
-install_java() {
-  # Extract Java tarball
-  tar -xf /tmp/java.tar.gz -C /opt/
-  # Set JAVA_HOME environment variable
-  echo 'export JAVA_HOME=/opt/java' >> /etc/profile
-  source /etc/profile
-}
-
-# Function to install Nexus
-install_nexus() {
-  # Extract Nexus tarball
-  tar -xf /tmp/nexus.tar.gz -C /opt/
-}
-
 # Main function
 main() {
-  # Install Java
   install_java
-
-  # Install Nexus
   install_nexus
 
-  # Start Nexus service
   systemctl start nexus
   systemctl enable nexus
 
-  # Configure Nexus with Jenkins
   configure_nexus_with_jenkins
+
+  echo "Installation and configuration completed."
 }
 
 # Execute main function
